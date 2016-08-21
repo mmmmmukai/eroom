3396741a90c617b34c8c470b60333ae4a608532d
Jonah Lin <yclin.tagaya@gmail.com>
2016-08-21 21:20:16 +0900

    blog

--- wp-content/themes/eroom/index.php
+++ wp-content/themes/eroom/index.php
@@ -6,14 +6,25 @@
 
 get_header();?>
 
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post();  ?>
-    <?php the_content(); /** コンテンツ */ ?>
-  <?php endwhile; else : ?>
-  <div class="post">
-    <h2>記事はありません</h2>
-    <p>お探しの記事は見つかりませんでした。</p>
+  <div id="wrapper">
+    <h2>BLOG</h2>
+    <main id="main70">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post();  ?>
+        <article class="article-1">
+          <h3><?php the_title();?></h3>
+          <section class="article-text width100">
+            <?php the_content(); /** コンテンツ */ ?>
+          </section>
+        </article>
+      <?php endwhile; else : ?>
+      <div class="post">
+        <h2>記事はありません</h2>
+        <p>お探しの記事は見つかりませんでした。</p>
+      </div>
+      <?php endif; ?>
+    </main>
   </div>
-  <?php endif; ?>
+
 
 
 <?php get_footer(); ?>
