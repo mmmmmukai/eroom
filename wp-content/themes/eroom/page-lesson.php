04699a29d0fd63e9af11e2917249ee5f3a8fa3d5
Jonah Lin <yclin.tagaya@gmail.com>
2016-08-21 20:44:09 +0900

    lesson

---
+++ wp-content/themes/eroom/page-lesson.php
@@ -0,0 +1,84 @@
+<?php 
+
+/****************************************
+  Template Name: lesson
+  lesson.html
+*****************************************/
+
+get_header();?>
+
+<!-- <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/cf.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/top.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=yes">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+      <script>
+         $(function(){
+            $("#header").load("html/header.html");
+            $("#footer").load("html/footer.html");
+            $("#nav").load("html/nav.html");
+          });
+
+
+      </script>
+
+    <title>ERoomへようこそ</title>
+  </head>
+
+  <body>
+    <header id="header">   
+    </header>
+   
+    <nav id="nav">
+    </nav> -->
+  
+   <div id="wrapper"> 
+   <main class="main-article">
+      <h2>LESSON</h2>
+      <article class="article-text width100">
+      <p><span>英会話カフェERoomでは、お客様が楽しんで英語を学んでいただけるように、
+様々なレッスンをご用意しております。</span></p><p>お気軽にご連絡ください。</p>
+      <p>■入会金など必要ありません</p>
+      <p>※お申し込み方法は↓をご確認ください<br>※各クラス３名以上で開催！！定員６名または１０名（クラスにより異なります）<br>※プリント代・消費税込の料金です。
+</p>
+      <p>★何ヶ月もスクールに通うのは面倒…<br>★初心者だからクラスに参加するのは不安…<br>★高い授業料・入学金は払いたくない…<br>↑こんな方にオススメです。↑
+</p>
+     </article>
+
+     <article class="article-text width100">
+      <section>
+       <h3>初心者限定「スペシャルディスカウントカンバセーション」</h3>
+       <p>初心者の方にもっと英語を身近なものに感じ、苦手意識とサヨナラして頂くための２時間カンバセーションクラスです♪</p>
+       <p>カンバセーション・早口言葉・ユーズフルフレーズ・英語でゲーム　etc…お勉強！と言うよりも英語に慣れて楽しみましょう！ということで盛り沢山の内容です。</p>
+       <p>同じレベルの方と一緒にレベルUPを目指しましょう！！</p>
+      </section>
+      <section>
+       <h3>Q : このクラスの初心者とはどのくらいのレベル？</h3>
+       <p>A : 英語は好きだけど、全然自信がない…<br> なんとか自己紹介くらいなら出来るけど…<br>文章が組み立てられず、単語しか出てこない…</p>
+       <p>↑このような方々が参加しています。</p> 
+      </section>   
+      <sectin>
+       <h3>料金は６，０００円</h3>
+       <p>　１回２時間×４回＝合計４回、８時間<br>（１回１，５００円、時間換算では１時間７５０円！）<br>レッスンスタートから続けて同じ曜日のクラス４回コース</p>
+       <p>例）１０月１３日スタートですと。。。<br>１回目 : 10/13　２回目 : 10/27<br>３回目 : 11/10　４回目 : 11/24<br>　計４回</p>
+       <p>どの月/日からスタートしてもOKです♪<br>※４回のレッスンが終わっても継続可能です。<br>継続のお申し込みはお早めに！！</p>
+       <p>⭐︎特徴 : 無理なく学べる２週間に一度ペース！<br>お好きなスタート日が選べます。<br>⭐︎料金 : ¥６，０００ー（4回のレッスンの合計金額）<br>⭐︎時間 : 木曜日クラス（19:30~21:30）<br>土曜日クラス（16:00~18:00）<br>⭐︎定員 : １０名<br>★クラスのスケジュールはカレンダーを参考にして下さい。</p>  
+      </section>
+     </article>
+  </main>
+  </div>
+  
+  <footer id="footer">
+  </footer>
+  </body>
+
+</html>
