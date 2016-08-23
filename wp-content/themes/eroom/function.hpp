e238509eb140b07b89c87b425c0fd2721d2a7ba7
Jonah Lin <yclin.tagaya@gmail.com>
2016-08-23 15:31:45 +0900

    nav

--- wp-content/themes/eroom/function.hpp
+++ wp-content/themes/eroom/function.hpp
@@ -1,14 +1,5 @@
 <?php
 
-//add_action( 'init', 'mytheme_init' );
-//if ( ! function_exists( 'mytheme_init' ) ) {
-//function mytheme_init() {
-//global $wp_rewrite;
-//$wp_rewrite->use_trailing_slashes = false;
-//$wp_rewrite->page_structure = $wp_rewrite->root . '%pagename%.html';
-// flush_rewrite_rules( false );
-//}
-//}
-
+ register_nav_menu('mainmenu', 'メインメニュー');
 
 ?>
