4f71b1d97d5cf7755a7172394f45196ee8dc0392
Jonah Lin <yclin.tagaya@gmail.com>
2016-08-23 15:24:49 +0900

    css

--- wp-content/themes/eroom/index.php
+++ wp-content/themes/eroom/index.php
@@ -12,7 +12,7 @@ get_header();?>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post();  ?>
         <article class="article-1">
           <a href="<?=the_permalink();?>"><h3><?php the_title();?></h3></a>
-          <p style="color:gray;">カテゴリ:<?php the_category(', ')?></p>
+          <p>カテゴリ:<span style="color:gray;"><?php the_category(', ')?></span></p>
           <section class="article-text width100">
             <?php the_content(); /** コンテンツ */ ?>
           </section>
